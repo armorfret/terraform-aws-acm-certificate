561323a349996b441b71477b78f314366521a31c
Les Aker <me@lesaker.org>
2019-07-11 19:14:34 +0000

    fix list handling for tf 0.12

--- main.tf
+++ main.tf
@@ -36,5 +36,5 @@ resource "aws_route53_record" "validation" {
 
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = "${aws_acm_certificate.this.arn}"
-  validation_record_fqdns = ["${aws_route53_record.validation.*.fqdn}"]
+  validation_record_fqdns = aws_route53_record.validation[*].fqdn
 }
